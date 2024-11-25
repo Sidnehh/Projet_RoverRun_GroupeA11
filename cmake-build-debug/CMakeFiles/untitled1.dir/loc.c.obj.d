0d60074007a239d2aaa3dc28854a3983941eb6f7
Sidnehh <SidneySanglier13pgmail.com>
2024-11-25 05:24:57 +0100

    f

--- cmake-build-debug/CMakeFiles/untitled1.dir/loc.c.obj.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/untitled1.dir/loc.c.obj: \
- C:\Users\Sidne\OneDrive\Bureau\Projet_RoverRun_GroupeA11\loc.c \
- C:\Users\Sidne\OneDrive\Bureau\Projet_RoverRun_GroupeA11\loc.h
--- tree.c
+++ tree.c
@@ -7,6 +7,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include "map.h"
 
 
 
@@ -47,74 +48,31 @@ t_move adjustMoveBasedOnTerrain(t_move chosen_move, t_position current_position,
     return chosen_move; // Si aucun ajustement n'est nécessaire
 }
 
-
-void build_from_node(t_node* parent, int nb_children, t_localisation curr_loc, t_map map, int branch_moves[], int total_moves)
+void build_from_node(t_node* parent, int nb_children, t_localisation curr_loc, t_map map, int proba_moves[], int total_proba)
 {
-    // Vérifications initiales
-    if (parent == NULL)
+    if (parent==NULL)
     {
-        printf("Conditions d'arrêt globales atteintes pour (%d, %d). Enfants restants : %d, Mouvements disponibles : %d.\n",
-               curr_loc.pos.x, curr_loc.pos.y, nb_children, total_moves);
         return;
     }
-    printf("Construction pour parent (%d, %d) avec %d enfants restants et %d mouvements disponibles.\n",
-           curr_loc.pos.x, curr_loc.pos.y, nb_children, total_moves);
-    // Boucle sur les enfants à créer
-    for (int i = 0; i < nb_children; i++)
+
+    t_node *temp_node;
+    t_move new_mov;
+    t_localisation new_pos;
+    t_move* moves = getRandomMoves(nb_children);
+
+    for(int i=0;i<nb_children;i++)
     {
-        // Tirage aléatoire d’un mouvement
-        int r = rand() % total_moves;
-        int type = 0;
-        while (r >= branch_moves[type])
+        new_pos = predictLocalisation(curr_loc, new_mov);
+        new_mov = adjustMoveBasedOnTerrain(new_mov = moves[i],new_pos.pos,map);
+
+        if(checkValidPosition(new_pos.pos,map))
         {
-            r -= branch_moves[type];
-            type++;
+            temp_node = create_node(new_mov, map.costs[new_pos.pos.x][new_pos.pos.y], nb_children - 1);
+            add_child(parent, temp_node);
+            build_from_node(temp_node, nb_children - 1, new_pos, map, );
         }
 
-        t_move new_mov = (t_move)type;
-        t_localisation new_pos = predictLocalisation(curr_loc, new_mov);
-
-        // Ajustement du mouvement en fonction du terrain
-        new_mov = adjustMoveBasedOnTerrain(new_mov, curr_loc.pos, map);
-        if (new_mov == -2)
-        {
-            printf("MARC est tombé dans une crevasse. Pas de nœud créé.\n");
-            continue;
-        }
-        else if (new_mov == STILL)
-        {
-            printf("Mouvement inutilisable, Marc reste immobile\n");
-        }
-
-        // Récupération du coût
-        int cost = map.costs[new_pos.pos.y][new_pos.pos.x];
-
-        // Création et ajout de l'enfant
-        if(checkValidPosition(curr_loc.pos, map))
-        {
-            t_node* child = create_node(new_mov, cost, nb_children - 1); // Utilisation de nb_children - 1 ici pour limiter la profondeur
-            if (child == NULL)
-            {
-                printf("Erreur : Impossible de créer un enfant.\n");
-                continue;
-            }
-            if (isValidLocalisation(new_pos.pos, map.x_max, map.y_max))
-            {
-                add_child(parent, child);
-                int child_branch_moves[7];
-                memcpy(child_branch_moves, branch_moves, sizeof(int) * 7);
-                child_branch_moves[type]--;
-
-                printf("Appel récursif pour enfant (%d, %d) avec %d enfants restants et %d mouvements disponibles.\n",
-                       new_pos.pos.x, new_pos.pos.y, nb_children - 1, total_moves - 1);
-
-                // Appel récursif
-                build_from_node(child, nb_children - 1, new_pos, map, child_branch_moves, total_moves - 1);
-            }
-        }
     }
-    printf("Création terminée pour ce parent (%d, %d). Enfants créés : %d\n",
-           curr_loc.pos.x, curr_loc.pos.y, parent->num_children);
 }
 
 t_tree* allocate_tree(int nb_movements)
